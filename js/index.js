78207f9d15a8675b76e6561a89f09155d90d235d
MartaPatrycja <105316228+MartaPatrycja@users.noreply.github.com>
2023-04-01 19:13:25 +0200

    cleared the code

--- js/index.js
+++ js/index.js
@@ -19,7 +19,6 @@ function updateTime() {
   
   function updateCity(event) {
     let cityTimeZone = event.target.value;
-    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
     citiesElement.innerHTML = `
