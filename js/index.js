5696bfff4555d07378ebcea7744b8c0fdfbb32db
MartaPatrycja <105316228+MartaPatrycja@users.noreply.github.com>
2023-04-01 21:18:30 +0200

    updated html delted a line from js

--- js/index.js
+++ js/index.js
@@ -31,14 +31,13 @@ function updateTime() {
     if (cityTimeZone === "current") {
       cityTimeZone = moment.tz.guess();
     }
-    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
     citiesElement.innerHTML = `
     <div class="city">
       <div>
         <h2>${cityName}</h2>
-        <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
       </div>
       <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
       "A"
